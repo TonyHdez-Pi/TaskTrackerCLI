490c2849bf4cf90a4cb3be6c88726a08a85beda4
Antonio Hernandez <tonyhdezpi@gmail.com>
2025-04-08 10:37:09 -0600

    Add function "markInProgress"

--- index.js
+++ index.js
@@ -5,19 +5,12 @@
 // List all tasks that are not done
 // List all tasks that are in progress
 
-// commander -> To create commands and options.
-// lib/task -> The task logic.
-// lib/storage -> To save the data.
-
-// The commands must follow this structure:
-// task-cli add "Buy groceries"
-// task-cli update 1 "Buy groceries and cook dinner"
-
 const fs = require("fs");
 const {
 	addTask,
 	updateTask,
 	deleteTask,
+	markInProgress,
 	markDone,
 	listTask,
 } = require("./lib/task.js");
@@ -48,6 +41,10 @@ switch (process.argv[2]) {
 		deleteTask(process.argv[3]);
 		break;
 	}
+	case "mark-progress": {
+		markInProgress(process.argv[3]);
+		break;
+	}
 	case "mark-done": {
 		markDone(process.argv[3]);
 		break;
@@ -61,20 +58,3 @@ switch (process.argv[2]) {
 		break;
 	}
 }
-
-// how to stop using node index.js add "task" and use task-cli add "task"
-// how to tell the user the structure it needs to follow for the commands (1 argument, 2 arguments, 3 arguments).
-
-// first two elements in th process.argv are the node path, and the second its the file path where the
-// curren command its being executed.
-// and all the arguments i pass to node when initiated will be added to the array from the position 2 and foward.
-
-// node index.js update 1 "buy groceries"
-// process.argv.forEach((argument) => {
-// this command will give me the following 3 arguments
-// update
-// 1
-// buy groceries
-// all thanks to the process.argv
-// 	console.log(argument);
-// });
